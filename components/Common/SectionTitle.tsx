ce8a34d1d9732c4a57068036d808cd2d834b6366
Jay Gajera <55349048+jaygajera940@users.noreply.github.com>
2023-08-20 20:29:45 +0530

    Update SectionTitle.tsx

--- components/Common/SectionTitle.tsx
+++ components/Common/SectionTitle.tsx
@@ -1,6 +1,6 @@
 const SectionTitle = ({
   title,
-  paragraph,
+  
   width = "570px",
   center,
   mb = "100px",
@@ -20,9 +20,7 @@ const SectionTitle = ({
         <h2 className="mb-4 text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[45px]">
           {title}
         </h2>
-        <p className="text-base !leading-relaxed text-body-color md:text-lg">
-          {paragraph}
-        </p>
+       
       </div>
     </>
   );
